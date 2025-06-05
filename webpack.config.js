ba4167ec643d22d9fe64fb2457ba21d74b987dae
Nicholas Sala <salanicholas.14@gmail.com>
2025-06-05 15:37:08 +0200

    Added config file example

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   mode: 'production',
   output: {
-    filename: 'main.js',
+    filename: 'my-globe.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
