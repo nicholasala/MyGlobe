76e7d4edf8029ec4ae0be119340117ec37256a23
Nicholas Sala <salanicholas.14@gmail.com>
2025-06-07 11:15:13 +0200

    Style fixes

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const MESH_COLOR = 0xffffff;
 export const LIGHT_COLOR = 0xffffff;
-export const AMBIENT_LIGHT_INTENSITY = 3.4;
+export const AMBIENT_LIGHT_INTENSITY = 3.2;
 export const IMAGES_Y_OFFSET = 0.14;
 export const MIN_CAMERA_DISTANCE = 1.5;
 export const MAX_CAMERA_DISTANCE = 2.9;
